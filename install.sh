db5474c5a064a2c7068a763046501bfb2aa753be
Luis Alberto Santana <jackboot7@gmail.com>
2017-03-30 15:50:31 -0600

    updated with bashrc and zsh

--- install.sh
+++ install.sh
@@ -6,3 +6,7 @@ ln -s $PWD/vim/vimrc ~/.vimrc
 
 # copy dotfile for zsh config
 ln -s $PWD/zsh/zshrc ~/.zshrc
+ln -s $PWD/zsh/oh-my-zsh ~/.oh-my-zsh
+
+# copy dotfile for bashrc
+ln -s $PWD/bashrc/bashrc ~/.bashrc
